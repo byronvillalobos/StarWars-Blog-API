1aaf500db2b87c01974c70fe6a555a4787bc733f
byronvillalobos <byronvillalobos@hotmail.com>
2021-04-06 02:06:58 +0000

    comments

--- src/models.py
+++ src/models.py
@@ -7,6 +7,45 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    registerDate = db.Column(db.String(250), nullable=False)
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    peopleId = db.Column(db.Integer, db.ForeignKey('People.id'))
+    planetsId = db.Column(db.Integer, db.ForeignKey('Planets.id'))
+    userId = db.Column(db.Integer, db.ForeignKey('User.id'))
+
+
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    residents = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.String(250), nullable=False)
+    
+
 
     def __repr__(self):
         return '<User %r>' % self.username
